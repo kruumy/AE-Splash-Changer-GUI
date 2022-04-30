a9e1ab743f004c9722a7f2a8a154423e52043b95
kruumy <48849543+kruumy@users.noreply.github.com>
2022-04-30 05:53:59 +0000

    Add files via upload

---
+++ AeSplashChanger/MainForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace AeSplashChanger
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.importbtn = new System.Windows.Forms.Button();
+            this.exportbtn = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.aefolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.importFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.splashLabel = new System.Windows.Forms.Label();
+            this.defaultsbtn = new System.Windows.Forms.Button();
+            this.Github = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // importbtn
+            // 
+            this.importbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.importbtn.FlatAppearance.BorderSize = 0;
+            this.importbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.importbtn.Location = new System.Drawing.Point(11, 28);
+            this.importbtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.importbtn.Name = "importbtn";
+            this.importbtn.Size = new System.Drawing.Size(117, 194);
+            this.importbtn.TabIndex = 0;
+            this.importbtn.Text = "Import";
+            this.importbtn.UseVisualStyleBackColor = false;
+            this.importbtn.Click += new System.EventHandler(this.importbtn_Click);
+            // 
+            // exportbtn
+            // 
+            this.exportbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.exportbtn.FlatAppearance.BorderSize = 0;
+            this.exportbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exportbtn.Location = new System.Drawing.Point(499, 28);
+            this.exportbtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.exportbtn.Name = "exportbtn";
+            this.exportbtn.Size = new System.Drawing.Size(120, 194);
+            this.exportbtn.TabIndex = 0;
+            this.exportbtn.Text = "Export";
+            this.exportbtn.UseVisualStyleBackColor = false;
+            this.exportbtn.Click += new System.EventHandler(this.exportbtn_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(136, 28);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(355, 245);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // importFileDialog
+            // 
+            this.importFileDialog.FileName = "importFileDialog";
+            // 
+            // splashLabel
+            // 
+            this.splashLabel.AutoSize = true;
+            this.splashLabel.Location = new System.Drawing.Point(131, 3);
+            this.splashLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.splashLabel.Name = "splashLabel";
+            this.splashLabel.Size = new System.Drawing.Size(206, 22);
+            this.splashLabel.TabIndex = 1;
+            this.splashLabel.Text = "Current Splash Screen:";
+            // 
+            // defaultsbtn
+            // 
+            this.defaultsbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.defaultsbtn.FlatAppearance.BorderSize = 0;
+            this.defaultsbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.defaultsbtn.Location = new System.Drawing.Point(499, 228);
+            this.defaultsbtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.defaultsbtn.Name = "defaultsbtn";
+            this.defaultsbtn.Size = new System.Drawing.Size(120, 45);
+            this.defaultsbtn.TabIndex = 0;
+            this.defaultsbtn.Text = "Defaults";
+            this.defaultsbtn.UseVisualStyleBackColor = false;
+            this.defaultsbtn.Click += new System.EventHandler(this.defaultsbtn_click);
+            // 
+            // Github
+            // 
+            this.Github.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.Github.FlatAppearance.BorderSize = 0;
+            this.Github.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Github.Location = new System.Drawing.Point(11, 228);
+            this.Github.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Github.Name = "Github";
+            this.Github.Size = new System.Drawing.Size(117, 45);
+            this.Github.TabIndex = 0;
+            this.Github.Text = "Github";
+            this.Github.UseVisualStyleBackColor = false;
+            this.Github.Click += new System.EventHandler(this.githubbtn_click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(635, 285);
+            this.Controls.Add(this.Github);
+            this.Controls.Add(this.defaultsbtn);
+            this.Controls.Add(this.splashLabel);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.exportbtn);
+            this.Controls.Add(this.importbtn);
+            this.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "After Effects Splash Screen Changer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button importbtn;
+        private System.Windows.Forms.Button exportbtn;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.FolderBrowserDialog aefolderBrowserDialog;
+        private System.Windows.Forms.OpenFileDialog importFileDialog;
+        private System.Windows.Forms.Label splashLabel;
+        private System.Windows.Forms.Button defaultsbtn;
+        private System.Windows.Forms.Button Github;
+    }
+}
+
---
+++ AeSplashChanger/MainForm.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AeSplashChanger
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public string dllpath;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckResourceHacker();
+            dllpath = GetDllDirectroy();
+            if (dllpath == "None")
+            {
+                dllpath = ShowSelectDir();
+            }
+            GetAeSplash();
+            CreateBackup();
+        }
+        private string GetDllDirectroy()
+        {
+            string ae22dir = @"C:\Program Files\Adobe\Adobe After Effects 2022\Support Files\AfterFXLib.dll";
+            string ae21dir = @"C:\Program Files\Adobe\Adobe After Effects 2021\Support Files\AfterFXLib.dll";
+            string ae20dir = @"C:\Program Files\Adobe\Adobe After Effects 2020\Support Files\AfterFXLib.dll";
+            string ae19dir = @"C:\Program Files\Adobe\Adobe After Effects 2019\Support Files\AfterFXLib.dll";
+            string ae18dir = @"C:\Program Files\Adobe\Adobe After Effects 2018\Support Files\AfterFXLib.dll";
+            string ae17dir = @"C:\Program Files\Adobe\Adobe After Effects 2017\Support Files\AfterFXLib.dll";
+            string ae16dir = @"C:\Program Files\Adobe\Adobe After Effects 2016\Support Files\AfterFXLib.dll";
+
+            if (File.Exists(ae22dir))
+            {
+                return ae22dir;
+            }
+            else if (File.Exists(ae21dir))
+            {
+                return ae21dir;
+            }
+            else if (File.Exists(ae20dir))
+            {
+                return ae20dir;
+            }
+            else if (File.Exists(ae19dir))
+            {
+                return ae19dir;
+            }
+            else if (File.Exists(ae18dir))
+            {
+                return ae18dir;
+            }
+            else if (File.Exists(ae17dir))
+            {
+                return ae17dir;
+            }
+            else if (File.Exists(ae16dir))
+            {
+                return ae16dir;
+            }
+            else
+            {
+                return "None";
+            }
+        }
+        private string ShowSelectDir()
+        {
+            aefolderBrowserDialog.ShowDialog();
+            string selectedPath = aefolderBrowserDialog.SelectedPath;
+            if (selectedPath == null || selectedPath.Contains("Support Files") || !selectedPath.Contains("After Effects"))
+            {
+                MessageBox.Show("Please Select Valid Directory", "Error");
+                Environment.Exit(0);
+            }
+
+            return selectedPath;
+        }
+        private void SetAeSplash()
+        {
+            SetPNGResourceHacker(dllpath);
+        }
+        private void GetAeSplash()
+        {
+            ExtractPNGResourceHacker(dllpath, "AE_SPLASH");
+
+            if (!File.Exists("AE_SPLASH.png"))
+            {
+                while (!File.Exists("AE_SPLASH.png"))
+                {
+                    Console.WriteLine("Waiting For Splash");
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+            pictureBox.ImageLocation = "AE_SPLASH.png";
+            pictureBox.Load();
+        }
+        private void CreateBackup()
+        {
+            if (!File.Exists(dllpath + ".original"))
+            {
+                File.Copy(dllpath, dllpath + ".original");
+            }
+        }
+        private void CheckResourceHacker()
+        {
+            if (!File.Exists("ResourceHacker.exe"))
+            {
+                MessageBox.Show("Could Not Find ResourceHacker.exe", "Error");
+                Environment.Exit(0);
+            }
+        }
+        private void exportbtn_Click(object sender, EventArgs e)
+        {
+            SetAeSplash();
+        }
+        private void importbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                importFileDialog.Filter = "PNG files (*.png)|*.png";
+                importFileDialog.ShowDialog();
+                string selectedPNG = importFileDialog.FileName;
+                pictureBox.Image = Image.FromFile(selectedPNG);
+                splashLabel.Text = "Selected Splash Screen";
+            }
+            catch { }
+
+        }
+        private void ExtractPNGResourceHacker(string inputName, string PNGNAME)
+        {
+            Process RH = new Process();
+
+            RH.StartInfo.FileName = "ResourceHacker";
+            RH.StartInfo.Arguments = "-open \"" + inputName + "\" -save " + PNGNAME + ".png -action extract -mask PNG," + PNGNAME;
+            RH.Start();
+        }
+        private void SetPNGResourceHacker(string inputName)
+        {
+            pictureBox.Image.Save("output.png");
+
+            Process RH = new Process();
+            RH.StartInfo.FileName = "ResourceHacker";
+            RH.StartInfo.UseShellExecute = true;
+            RH.StartInfo.Arguments = "-open \"" + inputName + "\" -save \"AfterFXLib.dll\" -action addoverwrite -resource \"output.png\" -mask PNG,AE_SPLASH";
+            RH.Start();
+
+            if (!File.Exists("AfterFXLib.dll"))
+            {
+                while (!File.Exists("AfterFXLib.dll"))
+                {
+                    Console.WriteLine("Waiting For dll");
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+
+            System.Threading.Thread.Sleep(3000);
+
+            //RH.Kill();
+
+            File.Replace("AfterFXLib.dll", dllpath, dllpath + ".bak");
+
+            MessageBox.Show("Exported New AfterFXLib.dll Successfully", "Notice");
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            pictureBox.Dispose();
+            pictureBox.Image.Dispose();
+            pictureBox.Image = null;
+            System.Threading.Thread.Sleep(100);
+            File.Delete("AE_SPLASH.png");
+            File.Delete("ResourceHacker.ini");
+            File.Delete("output.png");
+        }
+        private void githubbtn_click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("https://github.com/kruumy/AE-Splash-Changer-GUI");
+            }
+            catch (System.ComponentModel.Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (System.Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
+        }
+        private void defaultsbtn_click(object sender, EventArgs e)
+        {
+            File.Delete(dllpath);
+            File.Copy(dllpath + ".original", dllpath);
+            File.Delete("AE_SPLASH.png");
+            MessageBox.Show("Restored Defaults\nPlease Restart Program", "Notice");
+        }
+    }
+}
